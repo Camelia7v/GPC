0a6d192b851ed5c92244e32cc35c9e8679bd0409
Ana Potoroaca <53191102+Ana-poto@users.noreply.github.com>
2021-04-11 10:46:25 +0300

    tema 4

--- GPC-Tema4/t04 PotoroacaAnaMaria LupancuVioricaCamelia.cpp
+++ GPC-Tema4/t04 PotoroacaAnaMaria LupancuVioricaCamelia.cpp
@@ -58,50 +58,56 @@ public:
         linii = MAX_L;
         coloane = MAX_C;
 
-        d_l = (2.0 * (1.0 - epsilon)) / ((double)linii - 1);
-        d_c = (2.0 * (1.0 - epsilon)) / ((double)coloane - 1);
+        d_l = (2.0 - 2.0 * epsilon) / ((double)linii);
+        d_c = (2.0 - 2.0 * epsilon) / ((double)coloane);
         //printf("Value of d_l = %f\n", d_l);
         //printf("Value of d_c = %f\n", d_c);
     }
 
     void set_linii(int l) {
         if (l <= MAX_L)
-            linii = l;
+            this->linii = l;
+        updateDs();
     }
     void set_coloane(int c) {
         if (c <= MAX_C)
-            linii = c;
+            this->coloane = c;
+        updateDs();
     }
 
+    void updateDs() {
+        d_l = (2.0 - 2.0 * epsilon) / ((double)linii);
+        d_c = (2.0 - 2.0 * epsilon) / ((double)coloane);
+    }
     int get_linii() { return linii; }
     int get_coloane() { return coloane; }
     ~GrilaCarteziana() {}
 
     void afisareGrila() {
-        glColor3f(0.0, 0.0, 0.0); // negru
-        double x_c = -1.0 + epsilon;
-        double x_l = -1.0 + epsilon;
+        glColor3f(0.0, 0.0, 0.0); // negru\
 
-        for (double i = 0; i <= linii; i++)
+        double xmax = (double)dim / (double)dim + epsilon;
+        double ymax = (double)dim / (double)dim + epsilon;
+        for (double i = 0; i < coloane + 1; i++)
         {
             glBegin(GL_LINE_STRIP);
-            glVertex2f(x_l, -1.0 + epsilon);
-            glVertex2f(x_l, 1.0 - epsilon);
-            x_l += d_l;
+            glVertex2f(-1.0 / xmax, (2.0 / (double)coloane * i - 1) / ymax);
+            glVertex2f(1.0 / xmax, (2.0 / (double)coloane * i - 1) / ymax);
             glEnd();
         }
-        for (double j = 0; j <= coloane; j++)
+
+        for (double i = 0; i < linii + 1; i++)
         {
             glBegin(GL_LINE_STRIP);
-            glVertex2f(-1.0 + epsilon, x_c);
-            glVertex2f(1.0 - epsilon, x_c);
-            x_c += d_c;
+            glVertex2f((2.0 / (double)linii * i - 1) / xmax, -1.0 / ymax);
+            glVertex2f((2.0 / (double)linii * i - 1) / xmax, 1.0 / ymax);
             glEnd();
         }
     }
 
     void drawCircle(float x1, float y1, float r) {
-        glColor3f(0.0f, 0.5f, 1.0f); // baby blue
+        //glColor3f(0.0f, 0.5f, 1.0f); // baby blue
+        glColor3f(0.0, 0.0, 0.0); //negru
         glBegin(GL_TRIANGLE_FAN);
         glVertex2f(x1, y1);
 
@@ -128,12 +134,12 @@ public:
             return;
         }
 
-        cx = -1 + epsilon + 7.5 * d_c;
-        cy = -1 + epsilon + 7.5 * d_l;
+        cx = -1 + epsilon + 0.0 * d_c;
+        cy = -1 + epsilon + 0.0 * d_l;
 
         double x_GL = cx + i * d_c;
         double y_GL = cy + j * d_l;
-        float raza = 0.04;
+        float raza = 0.03;
 
         drawCircle((float)x_GL, (float)y_GL, raza);
     }
@@ -212,144 +218,139 @@ public:
     {
 
         std::vector<CPunct>  puncte;
-        puncte.push_back(CPunct(x - 0.5, y - 0.5));
-        puncte.push_back(CPunct(x + 0.5, y + 0.5));
-        puncte.push_back(CPunct(x + 0.5, y - 0.5));
-        puncte.push_back(CPunct(x - 0.5, y + 0.5));
-        /*puncte.push_back(CPunct(-x, -y));
-        puncte.push_back(CPunct(-x, y));
-        puncte.push_back(CPunct(x, -y));
-        */
-        if (x != y) {
-            /*      puncte.push_back(CPunct(y,x));*/
-            puncte.push_back(CPunct(y + 0.5, x + 0.5));
-            puncte.push_back(CPunct(y - 0.5, x - 0.5));
-            puncte.push_back(CPunct(y - 0.5, x + 0.5));
-            puncte.push_back(CPunct(y + 0.5, x - 0.5));
-
-            /*  puncte.push_back(CPunct(-y,-x));
-              puncte.push_back(CPunct(-y,x));
-              puncte.push_back(CPunct(y,-x));*/
-
+        puncte.push_back(CPunct(x, y));
+        for (int i = 1; i <= 3 / 2; i++)
+        {
+            puncte.push_back(CPunct(x - i, y));
+            puncte.push_back(CPunct(x + i, y));
         }
+
         return puncte;
     }
     std::vector<CPunct> AfisareCerc4(float R)
     {
         std::vector<CPunct>  puncte;
-        int x = 0, y = R;
+        std::vector<CPunct>  puncteSecundare;
+        int x = R;
+        int y = 0;
         int d = 1 - R;
-        int dE = 3, dSE = -2 * R + 5;
-        std::vector<CPunct> puncteSec = AfisarePuncteCerc3(x, y);
-        puncte.insert(std::end(puncte), std::begin(puncteSec), std::end(puncteSec));
-        while (y > x)
+        int dN = 3;
+        int dNW = -2 * R + 5;
+
+        puncte.push_back(CPunct(x, y));
+        puncteSecundare = AfisarePuncteCerc3(x, y);
+        puncte.insert(std::end(puncte), std::begin(puncteSecundare), std::end(puncteSecundare));
+
+        while (x > y)
         {
-            if (d < 0)
+            if (d > 0)
             {
-                d += dE;
-                dE += 2;
-                dSE += 2;
+                // NW
+                d += dNW;
+                dN += 2;
+                dNW += 4;
+                x--;
             }
             else
             {
-                d += dSE;
-                dE += 2;
-                dSE += 4;
-                y--;
+                // N
+                d += dN;
+                dN += 2;
+                dNW += 2;
             }
-            x++;
-            std::vector<CPunct> puncteSec = AfisarePuncteCerc3(x, y);
-            puncte.insert(std::end(puncte), std::begin(puncteSec), std::end(puncteSec));
+            y++;
+
+            puncte.push_back(CPunct(x, y));
+            puncteSecundare = AfisarePuncteCerc3(x, y);
+            puncte.insert(std::end(puncte), std::begin(puncteSecundare), std::end(puncteSecundare));
+
         }
         return puncte;
     }
 
-    std::vector<CPunct> UmplereElipsa(int x0, int y0, int a, int b, double val) {
-        std::vector<CPunct>  puncte;
-        int xi = 0, x = 0, y = b;
-        double fxpyp = 0.0;
-        double deltaE, deltaSE, deltaS;
 
-        puncte.push_back(CPunct(y + y0 + 0.5, xi + x0 + 0.5));
-        puncte.push_back(CPunct(y + y0 - 0.5, xi + x0 - 0.5));
-
-        val = x + x0;
-        // regiunea 1
-        while (a * a * (y - 0.5) > b * b * (x + 1))
-        {
-            deltaE = b * b * (2 * x + 1);
-            deltaSE = b * b * (2 * x + 1) + a * a * (-2 * y + 1);
-            if (fxpyp + deltaE <= 0.0)
-            {
-                // E este in interior
-                fxpyp += deltaE;
-                x++;
-                /*puncte.push_back(CPunct(y + y0 + 0.5, xi + x0 + 0.5));
-                puncte.push_back(CPunct(y + y0 - 0.5, xi + x0 - 0.5));*/
-                std::vector<CPunct> puncteSec = AfisarePuncteCerc3(y + y0, xi + x0);
-                puncte.insert(std::end(puncte), std::begin(puncteSec), std::end(puncteSec));
-
-
-                val = x + x0;
-            }
-            else if (fxpyp + deltaSE <= 0.0)
-            {
-                // SE este in interior
-                fxpyp += deltaSE;
-                x++; y--;
-                //puncte.push_back(CPunct(y + y0 + 0.5, xi + x0 + 0.5));
-                //puncte.push_back(CPunct(y + y0 - 0.5, xi + x0 - 0.5));
-                std::vector<CPunct> puncteSec = AfisarePuncteCerc3(y + y0, xi + x0);
-                puncte.insert(std::end(puncte), std::begin(puncteSec), std::end(puncteSec));
-
-                val = x + x0;
-            }
-            // regiunea 2
-            while (y > 0)
-            {
-                deltaSE = b * b * (2 * x + 1) + a * a * (-2 * y + 1);
-                deltaS = a * a * (-2 * y + 1);
-                if (fxpyp + deltaSE <= 0.0)
-                {
-                    // SE este in interior
-                    fxpyp += deltaSE;
-                    x++; y--;
-                }
-                else
-                {
-                    // S este in interior
-                    fxpyp += deltaS;
-                    y--;
-                }
-                /*puncte.push_back(CPunct(y + y0 + 0.5, xi + x0 + 0.5));
-                puncte.push_back(CPunct(y + y0 - 0.5, xi + x0 - 0.5));*/
-                std::vector<CPunct> puncteSec = AfisarePuncteCerc3(y + y0, xi + x0);
-                puncte.insert(std::end(puncte), std::begin(puncteSec), std::end(puncteSec));
-                val = x + x0;
-            }
-        }
-
-        return puncte;
-    }
-    void afisareCerc(float r, int width) {
-        std::vector<CPunct> pixeli = AfisareCerc4(r);
+    void afisareCerc(float  R, int width) {
+        std::vector<CPunct> pixeli = AfisareCerc4(R);
         for (int index = 0; index < pixeli.size(); index++)
         {
             //printf("Value of x = %f\n", punct.get_x());
             writePixel(pixeli[index].get_x(), pixeli[index].get_y());
         }
+        glColor3f(1.0, 0.1, 0.1); // rosu
+        glLineWidth(4.f);
+        double xmax = 1 + epsilon;
+        double ymax = 1 + epsilon;
+        float rx = (2.0 / (double)coloane * (0 + R) - 1) / xmax;
+        float ry = (2.0 / (double)linii * 0 - 1) / ymax;
+        glBegin(GL_LINE_LOOP);
+        for (int i = 0; i < 1000; i++)
+        {
+            float theta = 2.0f * 3.1415926f * float(i) / float(1000);//get the current angle
 
-        glBegin(GL_POLYGON);
-        double ori_x = 0.0;
-        double ori_y = 0.0;
-        for (int i = 0; i <= 300; i++) {
-            double angle = 2 * 4 * atan(1) * i / 300;
-            double x = cos(angle) * r * d_l;
-            double y = sin(angle) * r * d_c;
-            glVertex2d(ori_x + x, ori_y + y);
+            float x = (rx - cx) * cosf(theta);//calculate the x component
+            float y = (rx - cx) * sinf(theta);//calculate the y component
+
+            glVertex2f((double)x + cx, ((double)y + cy));//output vertex
+
         }
         glEnd();
+        glLineWidth(1.f);
 
+    }
+
+    std::vector<CPunct> UmplereElipsa(int x0, int y0, int a, int b, double val) {
+        std::vector<CPunct>  puncte;
+        int x = -a;
+        int y = 0;
+
+        double d2 = b * b * (x + 0.5) * (x + 0.5) + a * a * (y - 1) * (y - 1) - a * a * b * b;
+        double d1;
+
+        puncte.push_back(CPunct(x + x0, y + y0));
+        for (int i = x; i < 0; i++)
+            puncte.push_back(CPunct(x + x0 - i, y + y0));
+
+        while (a * a * (y - 0.5) > b * b * (x + 1))
+        {
+            if (d2 >= 0)
+            {
+                // selectam SE
+                d2 += b * b * (2 * x + 3) + a * a * (-2 * y + 2);
+                x++;
+                y--;
+            }
+            else
+            {
+                // selectam S
+                d2 += a * a * (-2 * y + 3);
+                y--;
+            }
+
+            for (int i = x; i < 0; i++)
+                puncte.push_back(CPunct(x + x0 - i, y + y0));
+        }
+
+        d1 = b * b * (x + 1) * (x + 1) + a * a * (y - 0.5) * (y - 0.5) - a * a * b * b;
+        while (y > -b)
+        {
+            if (d1 >= 0)
+            {
+                // selectam E
+                d1 += b * b * (2 * x + 3);
+                x++;
+            }
+            else
+            {
+                // selectam SE
+                d1 += b * b * (2 * x + 3) + a * a * (-2 * y + 2);
+                x++;
+                y--;
+            }
+
+            for (int i = x; i < 0; i++)
+                puncte.push_back(CPunct(x + x0 - i, y + y0));
+        }
+        return puncte;
     }
     void afisareElipsa(int x0, int y0, int a, int b, double val) {
         std::vector<CPunct> pixeli = UmplereElipsa(x0, y0, a, b, val);
@@ -358,41 +359,40 @@ public:
             //printf("Value of x = %f\n", punct.get_x());
             writePixel(pixeli[index].get_x(), pixeli[index].get_y());
         }
+
+
+        float raza = 0.03;
+        glColor3f(1.0, 0.1, 0.1); // rosu
+        glLineWidth(4.f);
+        glBegin(GL_LINE_STRIP);
+
+        for (float angle = 1.0f; angle < 361.0f; angle += raza)
+        {
+            //2.3 distanta la margini
+            float x2 = sin(angle) * raza * x0 * 2.3;
+            float y2 = cos(angle) * raza * y0 * 2.3;
+            glVertex2f(x2, y2 - .44);
+        }
+
+        glEnd();
+        glLineWidth(1.f);
+
+
     }
 };
 
 
 void Display1() {
     GrilaCarteziana grila;
-    //grila.linii = 16;
-    //grila.coloane = 16;
+    grila.set_linii(16);
+    grila.set_coloane(16);
     grila.afisareGrila();
     /* grila.afisareLinie(-7.5, 7.5, 7.5, 2.5);
      grila.afisareLinie(-7.5, -7.5, 7.5, -0.5);*/
-     /* grila.afisareCerc(7, 3);*/
-    grila.afisareElipsa(1, 1, 5.5, 5.5, 5);
+    grila.writePixel(0.0, 0.0);
 
-    //coordonate extreme grila 16x16
-    /*grila.writePixel(0, 0);
-    grila.writePixel(-7.5, -7.5);
-    grila.writePixel(-7.5, 7.5);
-    grila.writePixel(7.5, -7.5);
-    grila.writePixel(7.5,7.5);*/
-
-    //coordonate extreme grila 26x26
-
-    //stg jos
-    grila.writePixel(-7.5, -7.5);
-    //drp sus
-    grila.writePixel(17.5, 17.5);
-    //centru
-    grila.writePixel(5.5, 5.5);
-    //stg sus
-    grila.writePixel(-7.5, 17.5);
-    //drp jos
-    grila.writePixel(17.5, -7.5);
-
-
+    grila.afisareCerc(14, 3);
+    //grila.afisareElipsa(1, 1, 10, 10, 5);
 
     glFlush();
 }
@@ -412,7 +412,35 @@ void Init(void) {
 void Display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    Display1();
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    GrilaCarteziana grilaCarteziana1;
+    grilaCarteziana1.set_linii(16);
+    grilaCarteziana1.set_coloane(16);
+    GrilaCarteziana grilaCarteziana2;
+
+    switch (prevKey)
+    {
+    case '1':
+        grilaCarteziana1.afisareGrila();
+        grilaCarteziana1.afisareCerc(14, 3);
+        //grilaCarteziana1.writePixel(0, 0);
+        break;
+    case '2':
+        //centru elipsei 13,13
+        grilaCarteziana2.afisareGrila();
+        grilaCarteziana2.afisareElipsa(13, 7, 13, 7, 1);
+        break;
+        //case '3':
+        //    grilaCarteziana1.afisareGrila();;
+        //    grilaCarteziana1.deseneazaPoligon("poligon.txt");
+        //    break;
+    default:
+        break;
+    }
+
+    glFlush();
+    glFlush();
     glFlush();
 }
 
